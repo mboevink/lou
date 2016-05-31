112fbb637d577bb61cc714e6a1ff448982cb2ff1
LePetitMarill <d.brefort@hotmail.com>
2016-05-31 03:00:13 +0200

    Create lou.js

---
+++ lou.js
@@ -0,0 +1,12 @@
+function s(i) {
+	i = i.replace(/^(>.*)/gi, '<span style="color:#789922">$1</span>');
+	i = i.replace(/(?:https?:\/\/)?(?:www\.)?youtu(?:be\.com\/watch\?(?:.*?&(?:amp;)?)?v=|\.be\/)([\w\-]+)(?:&(?:amp;)?[\w\?=]*)?/gi, '<iframe width="480" height="360" src="https://www.youtube-nocookie.com/embed/$1?rel=0" frameborder="0" allowfullscreen></iframe>');
+	i = i.replace(/(\b(https?):\/\/(?!www\.youtube-nocookie\.com)[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi, '<a href="$1" target="_blank"> $1 </a>');
+	i = i.replace(/[^"](\b(https?):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|]\.(jpe?g|gif|png|bmp))[^"]/gi, '<img src="$1" />');
+	// i = i.replace(new RegExp('\\b(' + me.pseudo.fr + '|' + me.pseudo.en + ')\\b', 'gi'), '<strong style="color:' + me.color + '">$1</strong>');
+	return i;
+}
+$('tr').initialize(function() {
+    $(this).children('td:nth-of-type(2)').html(s($(this).children('td:nth-of-type(2)').text()));
+    $(this).children('td:nth-of-type(1)').click(function() { $('#input').val( $('#input').val() + $(this).text()); });
+});
