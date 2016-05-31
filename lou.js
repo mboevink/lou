c6af7176924a73f28b8b278b57ca9d72e23e9c59
LePetitMarill <d.brefort@hotmail.com>
2016-05-31 03:21:43 +0200

    scroll

--- lou.js
+++ lou.js
@@ -1,9 +1,12 @@
 function s(i) {
+	var j = i;
 	i = i.replace(/^(>.*)/gi, '<span style="color:#789922">$1</span>');
 	i = i.replace(/(?:https?:\/\/)?(?:www\.)?youtu(?:be\.com\/watch\?(?:.*?&(?:amp;)?)?v=|\.be\/)([\w\-]+)(?:&(?:amp;)?[\w\?=]*)?/gi, '<iframe width="480" height="360" src="https://www.youtube-nocookie.com/embed/$1?rel=0" frameborder="0" allowfullscreen></iframe>');
 	i = i.replace(/(\b(https?):\/\/(?!www\.youtube-nocookie\.com)[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi, '<a href="$1" target="_blank"> $1 </a>');
 	i = i.replace(/[^"](\b(https?):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|]\.(jpe?g|gif|png|bmp))[^"]/gi, '<img src="$1" />');
 	// i = i.replace(new RegExp('\\b(' + me.pseudo.fr + '|' + me.pseudo.en + ')\\b', 'gi'), '<strong style="color:' + me.color + '">$1</strong>');
+	if(j !== i && $('#chatbox')[0].scrollTop === ($('#chatbox')[0].scrollHeight - $('#chatbox')[0].offsetHeight))
+		setTimeout(function(){ $('#chatbox')[0].scrollTop = $('#chatbox')[0].scrollHeight; }, 100);
 	return i;
 }
 $('tr').initialize(function() {
