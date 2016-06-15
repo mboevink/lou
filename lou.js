a4bf83ea94acf77d2a830f3a90e0d5b777198a46
LePetitMarill <d.brefort@hotmail.com>
2016-06-15 02:04:28 +0200

    baseline

--- lou.js
+++ lou.js
@@ -1,3 +1,15 @@
+// ==UserScript==
+// @name         Lou++
+// @author       Marill
+// @match        https://loult.family/
+// @match        https://loult.family/*
+// @grant        none
+// @require      https://code.jquery.com/jquery-latest.js
+// @require      https://code.highcharts.com/highcharts.js
+// @require      https://code.highcharts.com/highcharts-more.js
+// @require      https://cdn.rawgit.com/adampietrasiak/jquery.initialize/master/jquery.initialize.js
+// ==/UserScript==
+
 var timestamps = [], actVal = 0, curVal = 0;
 function s(i) {
 	var j = i;
@@ -19,9 +31,12 @@ $('tr').initialize(function() {
     if($('#chatbox')[0].scrollTop - $('#chatbox')[0].scrollHeight > -1200) $('#chatbox').animate({scrollTop:$('#chatbox')[0].scrollHeight}, 100);
     timestamps.push(Date.now());
 });
+var baseline = ['Nuit gravement à la santé', 'Petits amours épistolaires', 'L\'intelligentsia française'];
+$('#nom').append('<small style="display:block;">« ' + baseline[Math.floor(Math.random() * baseline.length)] + ' »</small>');
 $('main').append('<div id="container" style="width:240px; height:240px; position:fixed; bottom:-50px; right:-27px;"></div>');
-$('head').append('<style type="text/css">header { background:url(http://static.tumblr.com/bogk4us/Xrklvfe3l/-pika.jpg) #f0e943 75% -200px no-repeat; text-align:center; } tr:nth-child(odd) td:nth-of-type(1) { background:#EEE; } tr:nth-child(odd) td:nth-of-type(2) { background:linear-gradient(to right, #EEE 0%, #f5f5f5 100%); } tr:nth-child(odd) td:nth-of-type(3) { background:#f5f5f5; } td:nth-of-type(1), td:nth-of-type(1) * { cursor:pointer; -webkit-touch-callout:none; -webkit-user-select:none; -khtml-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none; } #userlist, #input { background:#f5f5f5; } td:nth-of-type(2) img { max-width:100%; max-height:360px; } td:not(:last-child) { border-right:0; } #userlist div { border-bottom:0; } #nom { text-shadow:0 1px 1px #666; font-weight:bold; color:#FFF; position:absolute; left:185px; right:310px; } .log td:nth-of-type(2) { text-align:center; } #volrange { position:relative; top:-2px; }</style>');
+$('head').append('<style type="text/css">header { background:url(http://static.tumblr.com/bogk4us/Xrklvfe3l/-pika.jpg) #f0e943 75% -200px no-repeat; text-align:center; } tr:nth-child(odd) td:nth-of-type(1) { background:#EEE; } tr:nth-child(odd) td:nth-of-type(2) { background:linear-gradient(to right, #EEE 0%, #f5f5f5 100%); } tr:nth-child(odd) td:nth-of-type(3) { background:#f5f5f5; } td:nth-of-type(1), td:nth-of-type(1) * { cursor:pointer; -webkit-touch-callout:none; -webkit-user-select:none; -khtml-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none; } #userlist, #input { background:#f5f5f5; } td:nth-of-type(2) img { max-width:100%; max-height:360px; } td:not(:last-child) { border-right:0; } #userlist div:last-of-type { margin-bottom:190px; } #userlist div { border-bottom:0; } #sidebtns { padding-right:0; } #nom { text-shadow:0 1px 1px #666; padding:5px; font-weight:bold; color:#FFF; position:absolute; left:185px; right:310px; } .log td:nth-of-type(2) { text-align:center; } #volrange { position:relative; top:-2px; }</style>');
 $('header').prepend('<a href="LaMenuiserie" target="_blank" style="float:right; color:#000; text-decoration:none; padding:20px 10px;">🔨</a>');
+$('#prefs a').prepend('<img src="http://orig12.deviantart.net/32dd/f/2015/137/c/c/richard_stallman_approves_by_terrance8d-d8tq64f.png" />');
 $('#volrange').prependTo('#sidebtns');
 /* cre pce background:url(http://choualbox.com/Img/138417873956.jpg) center/cover; */
 /* background:linear-gradient(to bottom, #1e5799 0%,#7db9e8 100%); */
@@ -41,7 +56,7 @@ $(function () {
     },
     yAxis: {
       min: 0,
-      max: 500,
+      max: 600,
       minorTickInterval: 'auto',
       minorTickWidth: 1,
       minorTickLength: 5,
@@ -53,7 +68,7 @@ $(function () {
       tickLength: 10,
       tickColor: '#444',
       labels: {
-        step: 2,
+        step: 1,
         rotation: 'auto'
       },
       title: {
@@ -70,7 +85,7 @@ $(function () {
         color: '#DD0'
       }, {
         from: 300,
-        to: 500,
+        to: 600,
         color: '#D44'
       }]
     },
@@ -96,7 +111,7 @@ $(function () {
         else
             break;
       span = (timestamps.length > 1 ? timestamps[timestamps.length -1] - timestamps[0] : 60000);
-      newVal = Math.max(0, Math.min(((60000 * timestamps.length) / span), 500));
+      newVal = Math.max(0, Math.min(((60000 * timestamps.length) / span), 650));
       curVal = (timestamps.length > actVal ? newVal : curVal - (curVal * 0.01));
       actVal = timestamps.length;
       point.update(Math.round(curVal));
